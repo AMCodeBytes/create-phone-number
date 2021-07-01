2b488f63f123e7161adfbcd213bc26e25358dd82
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2021-07-01 14:42:15 +0100

    Initial code that passed

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const createPhoneNumber = (numbers) => {
-  
+	let result = '(';
+
+	for (let i = 0; i < numbers.length; i++) {
+		if (i < 3) {
+			result += i === 2 ? `${numbers[i]}) ` : numbers[i].toString();
+		} else {
+			result += i === 5 ? `${numbers[i]}-` : numbers[i].toString();
+		}
+	}
+
+	return result;
 }
 
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
